55ee8fb6558febf6f721b6f709582efa4d6352d6
Djoseffer <tom_djosefer@hotmail.com>
2024-07-04 16:09:01 -0300

    feat: Test insertion for database

--- src/main/java/com/springbootmongodb/domain/User.java
+++ src/main/java/com/springbootmongodb/domain/User.java
@@ -2,10 +2,13 @@ package com.springbootmongodb.domain;
 
 import com.sun.source.doctree.SerialDataTree;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+@Document(collection = "user")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,6 +18,7 @@ public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
