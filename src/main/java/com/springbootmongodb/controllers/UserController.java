bab8e76d8b2966f3fe1ee4416c71f95fdca79695
Djoseffer <tom_djosefer@hotmail.com>
2024-07-05 09:35:14 -0300

    feat: method getById created

--- src/main/java/com/springbootmongodb/controllers/UserController.java
+++ src/main/java/com/springbootmongodb/controllers/UserController.java
@@ -1,16 +1,17 @@
 package com.springbootmongodb.controllers;
 
 import com.springbootmongodb.domain.User;
+import com.springbootmongodb.dto.UserDTO;
 import com.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -18,9 +19,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-       List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> list = userService.findAll();
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
