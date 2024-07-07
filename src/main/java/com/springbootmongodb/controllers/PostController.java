4450ef6248fde5fbe9b39fb93896950c7c985215
Djoseffer <tom_djosefer@hotmail.com>
2024-07-07 14:15:38 -0300

    feat: method for different forms of queries

--- src/main/java/com/springbootmongodb/controllers/PostController.java
+++ src/main/java/com/springbootmongodb/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 
@@ -30,6 +31,25 @@ public class PostController {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/searchtitle")
+    public ResponseEntity<List<Post>> searchTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.searchTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
